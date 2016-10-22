d811b79aa54e1cecde6c5e0d2124f945fba73a10
Phong Phan <fongfan999@gmail.com>
2016-10-22 17:38:48 +0700

    Loading posts on ajax technique via category nav

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,15 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show]
   def show
-    @posts = @category.posts.near(location_address, 50).paginate(page: params[:page])
-    @categories = Category.all
+    @posts = @category.posts.near(location_address, 50).
+      paginate(page: params[:page])
 
     respond_to do |format|
-      format.html
-      format.js { render file: 'market/index' }
+      if params[:page]
+        format.js { render file: 'market/index' }
+      else
+        format.js
+      end
     end
   end
 
