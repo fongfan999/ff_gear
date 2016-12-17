b80b897e290214dcd7a1b4ab7938f25b305b9070
Phong Phan <fongfan999@gmail.com>
2016-12-17 22:31:38 +0700

    Created admin dashboard boxes

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,6 +2,10 @@ class Admin::ApplicationController < ApplicationController
   before_action :authorize_admin!
 
   def index
+    @total_users = User.count
+    @total_posts = Post.count
+    @total_categories = Category.count
+    @total_attachments = Attachment.count
     @top_users = User.order(sign_in_count: :desc).limit(5)
   end
 
